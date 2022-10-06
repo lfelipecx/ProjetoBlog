1aa0a5aaec15d14a3f2421808a819f81cb5504b2
Luiz Felipe <lfelipecx@gmail.com>
2022-10-06 19:19:07 -0300

    projeto blog com NodeJS e MongoDB

---
+++ app.js
@@ -0,0 +1,13 @@
+//Carregando módulos
+const express = require('express')
+const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+//Configurações
+
+
+//Rotas
+
+
+//Outros
