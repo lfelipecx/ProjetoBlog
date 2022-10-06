3221994ffaaac33161c08bfd515423003f1974b7
Luiz Felipe <lfelipecx@gmail.com>
2022-10-06 19:51:16 -0300

    configurando modulos e algumas rotas

--- app.js
+++ app.js
@@ -2,12 +2,27 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
+//const mongoose = require('mongoose')
+const app = express()
+const admin = require('./routes/admin')
+
 
 //Configurações
-
+    //Body-parser
+    app.use(bodyParser.urlencoded({extended: true}))
+    app.use(bodyParser.json())
+    //Handlebars
+    app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
+    app.set('view engine', 'handlebars')
+    //Mongoose
 
 //Rotas
+    app.use('/admin', admin)
+
 
 
 //Outros
+const PORT = 8081
+app.listen(PORT, ()=>{
+    console.log('Servidor rodando')
+})
