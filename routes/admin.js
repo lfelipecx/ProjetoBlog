35146d39f351ee333f04aaf66e42011511c28f39
Luiz Felipe <lfelipecx@gmail.com>
2022-10-10 14:48:04 -0300

    listando categorias

--- routes/admin.js
+++ routes/admin.js
@@ -15,7 +15,12 @@ router.get('/posts', (req, res) => {
 })
 
 router.get('/categorias', (req, res) => {
-    res.render('admin/categorias')
+    Categoria.find().lean().sort({date: 'desc'}).then((categorias)=>{
+        res.render('admin/categorias', {categorias: categorias})
+    })   .catch((erro) =>{
+        req.flash('error_msg', 'Houve um erro ao listar as categorias')
+        res.redirect('/admin')
+    }) 
 })
 
 router.get('/categorias/add', (req, res) => {
