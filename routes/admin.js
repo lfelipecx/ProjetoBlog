5dd8ed360c4402ba2db68c3fc4ea8ece22cbfe61
Luiz Felipe <lfelipecx@gmail.com>
2022-10-10 15:54:41 -0300

    editando categoria

--- routes/admin.js
+++ routes/admin.js
@@ -59,5 +59,34 @@ router.post('/categorias/nova', (req, res) => {
     
 })
 
+router.get('/categorias/edit/:id', (req, res) => {
+    Categoria.findOne({_id: req.params.id}).lean().then((categoria)=>{
+        res.render('admin/editcategorias', {categoria: categoria})
+    }).catch((erro) => {
+        req.flash('error_msg', 'Esta categoria não existe')
+        res.redirect('/admin/categorias')
+    })    
+})
+
+router.post('/categorias/edit', (req, res) => {
+    Categoria.findOne({_id: req.body.id}).then((categoria) => {
+
+        categoria.nome = req.body.nome
+        categoria.slug = req.body.slug
+
+        categoria.save().then(() => {
+            req.flash('success_msg', 'Categoria editada com sucesso!')
+            res.redirect('/admin/categorias')
+        }).catch((erro) => {
+            req.flash('error_msg', 'Erro ao salvar editação de categoria')
+            res.redirect('/admin/categorias')
+        })
+
+    }).catch((erro) => {
+        req.flash('error_msg', 'Erro ao editar categoria')
+        res.redirect('/admin/categorias')
+    })   
+})
+
 
 module.exports = router
