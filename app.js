1467b0da4e38b680481c3c03abd9d4f6589e750a
Luiz Felipe <lfelipecx@gmail.com>
2022-10-21 16:57:15 -0300

    ajustando autenticação de usuário

--- app.js
+++ app.js
@@ -28,10 +28,11 @@ require('./config/auth')(passport)
     app.use(passport.initialize())
     app.use(passport.session())
     app.use(flash())
-    //Middleware
+    //Middleware criando variaveis globais
     app.use((req, res, next) => {
         res.locals.success_msg = req.flash('success_msg')
         res.locals.error_msg = req.flash('error_msg')
+        res.locals.error = req.flash('error')
         next()
     })
     //Body-parser
