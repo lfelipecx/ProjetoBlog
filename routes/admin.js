798b4c0f7024cc7186bc993267ff4453f8cb91d7
Luiz Felipe <lfelipecx@gmail.com>
2022-10-21 17:25:15 -0300

    controlando acesso

--- routes/admin.js
+++ routes/admin.js
@@ -5,18 +5,19 @@ require('../models/Categoria')
 const Categoria = mongoose.model('categorias')
 require('../models/Postagem')
 const Postagem = mongoose.model('postagens')
+const {eAdmin} = require('../helpers/eAdmin')
 
 
 //ROTAS
-router.get('/', (req, res) => {
+router.get('/', eAdmin, (req, res) => {
     res.render('admin/index')
 })
 
-router.get('/posts', (req, res) => {
+router.get('/posts', eAdmin, (req, res) => {
     res.send('Página de posts')
 })
 
-router.get('/categorias', (req, res) => {
+router.get('/categorias', eAdmin, (req, res) => {
     Categoria.find().lean().sort({date: 'desc'}).then((categorias)=>{
         res.render('admin/categorias', {categorias: categorias})
     })   .catch((erro) =>{
@@ -25,11 +26,11 @@ router.get('/categorias', (req, res) => {
     }) 
 })
 
-router.get('/categorias/add', (req, res) => {
+router.get('/categorias/add', eAdmin, (req, res) => {
     res.render('admin/addcategorias')
 })
 
-router.post('/categorias/nova', (req, res) => {
+router.post('/categorias/nova', eAdmin, (req, res) => {
 
     //Validando formulario de categorias
     var erros = []
@@ -61,7 +62,7 @@ router.post('/categorias/nova', (req, res) => {
     
 })
 
-router.get('/categorias/edit/:id', (req, res) => {
+router.get('/categorias/edit/:id', eAdmin, (req, res) => {
     Categoria.findOne({_id: req.params.id}).lean().then((categoria)=>{
         res.render('admin/editcategorias', {categoria: categoria})
     }).catch((erro) => {
@@ -70,7 +71,7 @@ router.get('/categorias/edit/:id', (req, res) => {
     })    
 })
 
-router.post('/categorias/edit', (req, res) => {
+router.post('/categorias/edit', eAdmin, (req, res) => {
     Categoria.findOne({_id: req.body.id}).then((categoria) => {
 
         categoria.nome = req.body.nome
@@ -90,7 +91,7 @@ router.post('/categorias/edit', (req, res) => {
     })   
 })
 
-router.post('/categorias/deletar', (req, res) => {
+router.post('/categorias/deletar', eAdmin, (req, res) => {
     Categoria.deleteOne({_id: req.body.id}).then(() => {
         req.flash('success_msg', 'Categoria excluída com sucesso!')
         res.redirect('/admin/categorias')
@@ -100,7 +101,7 @@ router.post('/categorias/deletar', (req, res) => {
     })
 })
 
-router.get('/postagens', (req, res) => {
+router.get('/postagens', eAdmin, (req, res) => {
     Postagem.find().lean().sort({data:'desc'}).populate('categoria').then((postagens) =>{
         res.render('admin/postagens', {postagens: postagens})
     }).catch((erro) => {
@@ -110,7 +111,7 @@ router.get('/postagens', (req, res) => {
     
 })
 
-router.get('/postagens/add', (req, res) =>{
+router.get('/postagens/add', eAdmin, (req, res) =>{
     Categoria.find().lean().then((categorias) => {
         res.render('admin/addpostagem', {categorias: categorias})
     }).catch((erro) => {
@@ -119,7 +120,7 @@ router.get('/postagens/add', (req, res) =>{
     })    
 })
 
-router.post('/postagens/nova', (req, res) => {
+router.post('/postagens/nova', eAdmin, (req, res) => {
     var erros = []
 
     if(req.body.categoria == '0'){
@@ -148,7 +149,7 @@ router.post('/postagens/nova', (req, res) => {
 
 })
 
-router.get('/postagens/edit/:id', (req, res) => {
+router.get('/postagens/edit/:id', eAdmin, (req, res) => {
 
     Postagem.findOne({_id: req.params.id}).lean().then((postagem) => {
 
@@ -165,7 +166,7 @@ router.get('/postagens/edit/:id', (req, res) => {
     })    
 })
 
-router.post('/postagem/edit', (req, res) => {
+router.post('/postagem/edit', eAdmin, (req, res) => {
 
     Postagem.findOne({_id: req.body.id}).then((postagem) => {
 
@@ -189,7 +190,7 @@ router.post('/postagem/edit', (req, res) => {
     })
 })
 
-router.get('/postagens/deletar/:id', (req, res) => {
+router.get('/postagens/deletar/:id', eAdmin, (req, res) => {
     Postagem.remove({_id: req.params.id}).then(() => {
         req.flash('success_msg', 'Postagem excluída com sucesso!')
         res.redirect('/admin/postagens')
