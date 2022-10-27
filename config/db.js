50f85d37cd6a1676960ea30516c3ad03605c2207
Luiz Felipe <lfelipecx@gmail.com>
2022-10-27 15:02:53 -0300

    teste conect heroku

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV == 'production'){
     module.exports = {
-        mongoURI: 'mongodb+srv://luizfelipe:lui30589@blogapp.b3i3yzc.mongodb.net/?retryWrites=true&w=majority'        
+        mongoURI: 'mongodb+srv://luizfelipe:lui30589@blogapp.b3i3yzc.mongodb.net/test'        
     }
 }else{
     module.exports = {
